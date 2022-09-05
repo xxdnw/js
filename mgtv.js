48f1c4c0dbe3a815548be53e59b238fff67cee10
xxdnw <89925304+xxdnw@users.noreply.github.com>
2022-09-06 01:23:25 +0800

    Update mgtv.js

--- mgtv.js
+++ mgtv.js
@@ -22,7 +22,7 @@ https://mdwy5.com/#/register?code=580580
 [rewrite_local]
 
 ^https:\/\/fcapi\.91por\.vip\/api\/video\/user\/info url script-response-body 
-mgtv.js
+https://raw.githubusercontent.com/xxdnw/js/main/mgtv.js?token=GHSAT0AAAAAABYNP7UFOZIFJ5UF2C56QSDOYYWGCXQ
 
 
 
